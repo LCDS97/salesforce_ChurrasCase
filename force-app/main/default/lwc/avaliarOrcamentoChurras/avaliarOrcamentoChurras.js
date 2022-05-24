f8f2ee5a3ae97b5b5c6c8e395ce85cfc8abf8c34
lcds97 <lucas.cds1997@gmail.com>
2022-05-24 17:08:42 -0300

    Backend do Componente

--- force-app/main/default/lwc/avaliarOrcamentoChurras/avaliarOrcamentoChurras.js
+++ force-app/main/default/lwc/avaliarOrcamentoChurras/avaliarOrcamentoChurras.js
@@ -1,3 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
-export default class AvaliarOrcamentoChurras extends LightningElement {}
+import buscarAvaliacoesPorOrcamentoService from '@/salesforce/apex/AvaliarChurrasController.buscarAvalicoesPorOrcamentoChurras';
+
+export default class AvaliarOrcamentoChurras extends LightningElement {
+
+    @api recordId;
+
+    @track lstAvalicaoes = [];
+    @track lstAvalicaoesFiltradas = [];
+    @track apresentarAvaliacoes = true;
+
+    connectedCallback(){
+        this.carregarAvaliacoes(this.recordId);
+    }
+
+    carregarAvaliacoes(idOrcamentoChurras){
+        buscarAvaliacoesPorOrcamentoService({ idOrcamentoChurras })
+            .then(response => {
+                if(!response.sucesso || response.lstObject.length == 0){
+                    this.apresentarAvaliacoes = false;
+
+                }
+
+                this.lstAvalicaoes = response.lstAvalicaoes;
+                this.lstAvalicaoesFiltradas = response.lstObject;
+            })
+            .catch(response => {
+                console.log(error);
+            })
+    }
+
+
+}
